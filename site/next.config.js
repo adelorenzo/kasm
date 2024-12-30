3aef3ad773e4555e2198878fead883fbedafce28
adelorenzo <adolfo@delorenzo.mobi>
2024-12-29 21:40:26 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'adelorenzo Technologies',
+    description: 'The official store for adelorenzo kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://adelorenzo.github.io/kasm/',
+    contactUrl: 'https://adelorenzo.github.io/kasm/support',
   },
   reactStrictMode: true,
   swcMinify: true,
